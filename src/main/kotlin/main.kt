9747ae7693ef349a2cc2d3d4ea84bd3c3aa01c61
isso0424 <isso04240129@gmail.com>
2019-10-23 01:10:43 +0900

    fix warning

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,12 +1,9 @@
 fun main(){
-    val nikusutaHiyashi:Ramen = Ramen("うまい", "ﾁｮｯﾄﾀｶｲ", "満足/多い")
+    val nikusutaHiyashi = Ramen("うまい", "ﾁｮｯﾄﾀｶｲ", "満足/多い")
     nikusutaHiyashi.detail()
 }
 
-class Ramen(taste: String, price: String, volume: String){
-    private var taste: String = taste
-    private var price: String = price
-    private var volume: String = volume
+class Ramen(private var taste: String, private var price: String, private var volume: String){
     fun detail(){
         println(taste)
         println(price)
